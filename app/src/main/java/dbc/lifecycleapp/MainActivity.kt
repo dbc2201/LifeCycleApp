fb2bdcb5dcf6f1ecd32e9cf6fb0c7eccb3142609
dbc2201 <divyanshb22@gmail.com>
2020-09-25 22:00:46 +0530

    show a Toast message when the onCreate() function is called

--- app/src/main/java/dbc/lifecycleapp/MainActivity.kt
+++ app/src/main/java/dbc/lifecycleapp/MainActivity.kt
@@ -1,11 +1,17 @@
 package dbc.lifecycleapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(
+            this,
+            "onCreate() method is called",
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
